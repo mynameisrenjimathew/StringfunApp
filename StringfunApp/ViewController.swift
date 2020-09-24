8a2350b12d3789d2cb05967f11abbc12d278da36
renji <mynameisrenjimathew@gmail.com>
2020-09-24 17:48:28 +0530

    String palindrom

--- StringfunApp/ViewController.swift
+++ StringfunApp/ViewController.swift
@@ -9,9 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var textViews: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        textViews.isEditable = false
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +23,41 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    var lastLetter: String = ""
+    @IBAction func enterButton(_ sender: UIButton) {
+        
+        let mText: String = textField.text!
+        textViews.text = "String:\(mText ) \nReverse: \(reverse(mText))\nFrist Letter: \(mText.characters.first ?? "N")"
+        if isPalindrome(inputString: mText){
+            textViews.text! += "\nLast letter \(mText.characters.last ?? "N") \nIt is palindrome"
+        }else{
+            textViews.text! += "\nLast letter \(mText.characters.last ?? "N")\nIt is not palindrome"
+        }
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    func reverse(_ s: String) -> String {
+        var str = ""
+        for character in s.characters {
+            str = "\(character)" + str
+        }
+        return str
+    }
+    
+    func isPalindrome(inputString: String) -> Bool {
+        if reverse(inputString) != inputString {
+            return false
+        }
+        return true
+    }
 
 
+}
+extension ViewController : UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+}
 }
 
